c823a2f9ee987d56249805434ed11e3a5742605c
Alexandra D <alexandrad@alexandras-air.myfiosgateway.com>
2021-04-15 00:39:32 -0400

    cleaned up files

--- sketch.js
+++ sketch.js
@@ -1,83 +1,95 @@
 let boxes = [];
 let timer = 0;
-let hue = 300; // oscillate between 270 and 330 (purple and pink)
+let hue = 325; // oscillate between 310 and 340 (purple and pink)
 let hueCounter = 0;
-let world, plane, path, sky;
+let fontHue = 85; // oscillate between 50 and 120 (yellow and green)
+let fontHueCounter = 0;
+let maxSize = 600;
+let myFont;
+let audio;
+let clicked = false;
+let platform;
+
+function preload() {
+	myFont = loadFont("fonts/TrainOne-Regular.ttf");
+	audio = loadSound("Dragon Fruit Master 2021.m4a");
+	platform = loadImage("platform.png");
+}
 
 function setup() {
-	createCanvas(800, 800).id("canvas");
-	world = new World("VRScene");
-
-	plane = new Plane({
-		x: 0, y: 1, z: 0,
-		width: 10, height: 10,
-		asset: "canvas"
-	});
-	world.add(plane);
-
-	path = new Box({
-		x: 0, y: 1, z: 2,
-		width: 0.5, height: 0.2, depth: 5,
-		red: 100, green: 100, blue: 100
-	});
-	world.add(path);
-
-	sky = new Sphere({
-		x:0, y: 0, z: 0,
-		radius: 200,
-		red: 0, green: 0, blue: 0
-	});
-	world.add(sky);
-
-	world.setFlying(true);
+	createCanvas(window.innerWidth, window.innerHeight);
 
 	rectMode(CENTER);
+	imageMode(CENTER);
 	colorMode(HSB, 360, 100, 100);
 	strokeWeight(3);
-	noFill()
+	noFill();
+	textFont(myFont);
+	textAlign(CENTER);
+	textSize(64);
 }
 
 function draw() {
-	background(0, 0, 0, 0.1);
+	background(0, 0.1);
 
-	if (millis() - timer > 1000) {
-		// oscillate the color between 270 and 330 (purple and pink)
-		hueCounter += 0.3;
-		let newHue = hue + sin(hueCounter) * 30;
+	drawSquares(clicked);
+	image(platform, width/2 - 6, height*3/4, 500, height*0.78)
+	// drawRunway();
 
-		let temp = new Square(width/2, height/2, newHue);
-		boxes.push(temp);
-		timer = millis();
-	}
-
-	for (let i = 0; i < boxes.length; i++) {
-		boxes[i].display();
-		if (boxes[i].size > width) {
-			boxes.splice(i, 1);
-			i--;
-		}
-	}
-
-	plane.updateTexture();
-
-	// setGradient(width/4, height/2, width/2, height/2, color1, color2);
-	// erase();
-	// triangle(width/2, height/2, width/4, height, width*3/4, height);
-	// noErase();
-
-	// fill(0);
-	// noStroke();
-	// triangle(width/4, height/2, width/2 - 25, height/2, width/4, height);
-	// triangle(width*3/4, height/2, width/2 + 25, height/2, width*3/4, height);
+	fontHueCounter += 0.01;
+	let newFontHue = fontHue + sin(fontHueCounter) * 35;
+	fill(newFontHue, 80, 100);
+	noStroke();
+	text("Dragon Fruit", width/2, height/4)
 }
 
-// function setGradient(x, y, w, h, c1, c2) {
-// 	for (let i = y; i <= y + h; i++) {
-//     	let inter = map(i, y, y + h, 0, 1);
-//     	let c = lerpColor(c1, c2, inter);
-//     	stroke(c);
-//     	line(x, i, x + w, i);
-// 	}
+
+function mousePressed() {
+	clicked = !clicked;
+	if (!audio.isPlaying()) {
+		audio.play();
+	}
+	else {
+		audio.stop();
+	}
+}
+
+function drawSquares(audioStarted) {
+	if (audioStarted) {
+		if (millis() - timer > 857) {
+			// oscillate the color between 310 and 340 (purple and pink)
+			hueCounter += 0.5;
+			let newHue = hue + sin(hueCounter) * 15;
+
+			let temp = new Square(width/2, height/2, newHue);
+			boxes.push(temp);
+			timer = millis();
+		}
+
+		for (let i = 0; i < boxes.length; i++) {
+			boxes[i].display();
+			if (boxes[i].size > maxSize) {
+				boxes.splice(i, 1);
+				i--;
+			}
+	}
+	}
+}
+
+// function drawRunway() {
+// 	fill(0, 0, 70, 0.95);
+// 	noStroke();
+// 	beginShape();
+// 	vertex(width/2 - 150, height - 30);
+// 	vertex(width/2 - 150, height - 60);
+// 	vertex(width/2 - 20, height/2);
+// 	vertex(width/2 + 20, height/2);
+// 	vertex(width/2 + 150, height - 60);
+// 	vertex(width/2 + 150, height - 30);
+// 	endShape();
+
+// 	fill(0, 0, 50);
+// 	rect(width/2, height - 45, 300, 30);
 // }
 
 
@@ -91,7 +103,7 @@ class Square {
 	}
 
 	display() {
-		noFill()
+		noFill();
 		stroke(this.hue, 100, 100);
 
 		push();
